0b9732792b44f0311714df74fbb81005e2497dc5
DonAxis <alexis-axis@hotmail.com>
2026-02-07 11:15:37 -0600

    grupoId2

--- sice/coordinaModules.js
+++ sice/coordinaModules.js
@@ -3050,7 +3050,7 @@ async function guardarTodasCalificacionesCoord() {
             await db.collection('calificaciones').doc(docId).set({
                 alumnoId: alumno.id,
                 materiaId: asignacionCalifActual.materiaId,
-              //  grupoId: asignacionCalifActual.grupoId || null,  // ✅ Previene undefined
+                grupoId: asignacionCalifActual.grupoId || null,  // ✅ Previene undefined
                 profesorId: asignacionCalifActual.profesorId,
                 periodo: asignacionCalifActual.periodo,
                 parciales: {
