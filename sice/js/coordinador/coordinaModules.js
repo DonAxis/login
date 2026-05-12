7bffbaad1072d4fc2c94106caedc7263302aeb4f
DonAxis <alexis-axis@hotmail.com>
2026-05-11 22:58:29 -0600

    super 9

--- sice/js/coordinador/coordinaModules.js
+++ sice/js/coordinador/coordinaModules.js
@@ -2759,9 +2759,6 @@ function aplicarFiltrosAlumnos() {
                 </div>
                 <div class="item-acciones">
                     <button onclick="editarAlumno('${id}')" class="btn-editar">Editar</button>
-                    <button onclick="avanzarAlumnoIndividual('${id}')" class="botVerde" title="Avanzar al siguiente periodo sin cambio masivo">
-                        Avanzar Periodo
-                    </button>
                     <button onclick="toggleActivoUsuario('${id}', 'alumno', ${!alumno.activo})" class="botAzu">
                         ${alumno.activo ? 'Desactivar' : 'Activar'}
                     </button>
